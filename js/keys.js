c6ec70f3d0142919df88bd66090d27198006499d
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2024-01-11 14:42:32 -0500

    import keys

---
+++ js/keys.js
@@ -0,0 +1,5 @@
+ const MAPBOX_KEY = 'pk.eyJ1Ijoiemx1aWdvbiIsImEiOiJjbGYyb2oyZWswa2l1M3RvODRxenJ2MWVoIn0.BlJ22abmg_RrUUe0bvMFSg';
+
+const OPEN_WEATHER_APPID = "66b3d92058d86d07779d95a7faab6b24";
+
+const gitHubAPI_token = "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";
